b7ae0bd5444ded2059e4f2a6950e569feebf7fb4
BlakeBrown03 <blake.m.brown03@gmail.com>
2024-12-26 17:05:50 -0600

    Add new components for AboutMe, Contact, Projects, and Technologies; update App component and package dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Technologies from "./components/Technologies";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<AboutMe />
+			<Technologies />
+			<Projects />
+			<Contact />
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,16 @@
+export default function AboutMe() {
+	return (
+		<div>
+			<h1>About Me</h1>
+			<p>
+				I am a Computer Science student at the University of Wisconsin -
+				Madison and have a passion for web-development and the rapidly
+				evolving technologies that come with the field. I am graduating
+				in May of 2025 and will continue to work in this field. I take
+				user experience into account while writing reusable and
+				efficient code. I love combining new technologies, good design
+				and a bit of my personality into all of my projects.{" "}
+			</p>
+		</div>
+	);
+}
