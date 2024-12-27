0f2f6ceb7c836827024f48eaefcd9eb9ed1365d8
BlakeBrown03 <blake.m.brown03@gmail.com>
2024-12-27 01:40:25 -0600

    Enhance Contact page layout and functionality; update headings, add submit button with mailto link

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -10,25 +10,35 @@ export default function Contact() {
 			<h1 className="text-center font-bold text-4xl my-3">Contact</h1>
 			<div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 				<div className="bg-gray-200 p-4 rounded-lg">
-					<h2>Get in touch</h2>
-					<h2>Name:</h2>
+					<h1 className="text-center font-bold text-lg">
+						Get in touch
+					</h1>
+					<h2 className="my-1">Name:</h2>
 					<input
 						type="text"
 						onChange={e => setName(e.target.value)}
 						className="w-full p-2 border border-gray-300 rounded-lg"
 					/>
-					<h2>Email:</h2>
+					<h2 className="my-1">Email:</h2>
 					<input
 						type="email"
 						onChange={e => setEmail(e.target.value)}
 						className="w-full p-2 border border-gray-300 rounded-lg"
 					/>
-					<h2>Message:</h2>
+					<h2 className="my-1">Message:</h2>
 					<textarea
 						onChange={e => setMessage(e.target.value)}
 						className="w-full p-2 border border-gray-300 rounded-lg"
 						rows={5}
 					/>
+					<div className="flex justify-end">
+						<button className="bg-blue-500 text-white p-2 rounded-lg mt-2 ">
+							<a
+								href={`mailto:blake.m.brown03@gmail.com?subject=Contact%20Form&body=Name:%20${name}%0D%0AEmail:%20${email}%0D%0AMessage:%20${message}`}>
+								Submit
+							</a>
+						</button>
+					</div>
 				</div>
 				<div>
 					<h2>Email</h2>
