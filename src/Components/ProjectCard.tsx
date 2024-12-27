f8c568773d1f379430a21bbc1a4b71efee8e95b4
BlakeBrown03 <blake.m.brown03@gmail.com>
2024-12-27 01:08:44 -0600

    Add images to ProjectCard component and update project image paths in JSON

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -2,6 +2,16 @@ export default function ProjectCard(props: any) {
 	return (
 		<div className="container mx-auto col-span-1 border-gray-200 border-2 p-3 my-3 rounded-2xl shadow-md bg-gray-200 text-center">
 			<h1>{props.title}</h1>
+			<img
+				src={props.image}
+				alt={props.title}
+				style={{
+					width: "auto",
+					height: "auto",
+					maxHeight: "250px"
+				}}
+				className="mx-auto"
+			/>
 			<a href={props.link}>{props.linkText}</a>
 			<p>{props.description}</p>
 		</div>
