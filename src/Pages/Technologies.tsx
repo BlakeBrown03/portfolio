719a57c211c244ce45f4b584446f1583c928df1a
BlakeBrown03 <blake.m.brown03@gmail.com>
2024-12-26 18:46:04 -0600

    Refactor Technologies component: move technology data to JSON file and update rendering logic

--- src/Pages/Technologies.tsx
+++ src/Pages/Technologies.tsx
@@ -1,21 +1,13 @@
 import TechnologyCard from "../Components/TechnologyCard";
+import technologies from "../technologies.json";
 
 export default function Technologies() {
-	let technologies = [
-		{ picture: "src/logos/React-icon.svg", title: "React" },
-		{
-			picture: "src/logos/Typescript_logo_2020.svg",
-			title: "TypeScript"
-		},
-		{ picture: "src/logos/Bootstrap_logo.svg", title: "Bootstrap" }
-	];
-
 	return (
 		<div className="container mx-auto">
 			<h1 className="text-center font-bold text-4xl my-3">
 				Technologies and Tools
 			</h1>
-			<div className="grid auto-rows-auto auto-cols-min-1 auto-cols-max-4 gap-x-4 gap-y-4">
+			<div className="grid-flow-col-dense gap-x-4 gap-y-4">
 				{technologies.map((technology, index) => {
 					return <TechnologyCard key={index} {...technology} />;
 				})}
