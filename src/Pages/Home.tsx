f943c38931a327166d9da6ce15f93626d7bfd970
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-01-07 18:18:01 -0600

    Add overlay text to Home component with video background

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
 	return (
-		<div id="home">
+		<div id="home" className="relative">
 			<video autoPlay muted loop className={"w-full aspect-video"}>
 				<source src="src/batman.mp4" />
 			</video>
+			<h1 className="absolute inset-0 flex items-center justify-center z-1 text-white">
+				Blake Brown
+			</h1>
 		</div>
 	);
 }
