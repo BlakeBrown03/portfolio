a7ad02bb02757f2551086b9e6afe647f1c024424
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-01-07 18:36:27 -0600

    Refactor Home component to improve video display and overlay text styling

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
 	return (
-		<div id="home" className="relative">
-			<video autoPlay muted loop className={"w-full aspect-video"}>
+		<div id="home" className="relative h-screen">
+			<video autoPlay muted loop className={"w-full h-full object-cover"}>
 				<source src="src/batman.mp4" />
 			</video>
-			<h1 className="absolute inset-0 flex items-center justify-center z-1 text-white">
+			<h1 className="absolute inset-0 flex items-center justify-center z-10 text-white text-7xl mix-blend-overlay">
 				Blake Brown
 			</h1>
 		</div>
