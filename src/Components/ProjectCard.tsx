87bb531cb83bea29299630412f7c3da9c71ffb97
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-01-11 02:44:16 -0600

    Fix dark mode styling for ProjectCard component by correcting class syntax

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,6 +1,6 @@
 export default function ProjectCard(props: any) {
 	return (
-		<div className="container mx-auto col-span-1 border-gray-200 border-2 p-3 my-3 rounded-2xl shadow-md bg-gray-200 text-center dark: bg-gray-800 border-gray-800">
+		<div className="container mx-auto col-span-1 border-gray-200 border-2 p-3 my-3 rounded-2xl shadow-md bg-gray-200 text-center dark:bg-gray-800 dark:border-gray-800">
 			<h1>{props.title}</h1>
 			<img
 				src={props.image}
