9b9197d860ccdba858bf370f39217a90c1abb536
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-01-11 18:40:51 -0600

    Enhance ProjectCard component with hover effect for description and update button styling

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -2,18 +2,24 @@ export default function ProjectCard(props: any) {
 	return (
 		<div className="container mx-auto col-span-1 border-gray-200 border-2 p-3 my-3 rounded-2xl shadow-md bg-gray-200 text-center dark:bg-gray-800 dark:border-gray-800">
 			<h1>{props.title}</h1>
-			<img
-				src={props.image}
-				alt={props.title}
-				style={{
-					width: "auto",
-					height: "auto",
-					maxHeight: "250px"
-				}}
-				className="mx-auto"
-			/>
-			<a href={props.link}>{props.linkText}</a>
-			<p>{props.description}</p>
+			<div className={"my-2 relative group"}>
+				<img
+					src={props.image}
+					alt={props.title}
+					style={{
+						width: "auto",
+						height: "auto",
+						maxHeight: "250px"
+					}}
+					className="mx-auto"
+				/>
+				<p className="opacity-0 group-hover:opacity-100 absolute inset-0 bg-gray-200 flex items-center text-black bg-opacity-80">
+					{props.description}
+				</p>
+			</div>
+			<button className="dark:bg-slate-400 dark:border-slate-400 rounded-md px-2 py-2 my-2">
+				<a href={props.link}>{props.linkText}</a>
+			</button>
 		</div>
 	);
 }
