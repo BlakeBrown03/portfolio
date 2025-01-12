0f2a0a5a268e8d98eab778f40efd056691fd014c
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-01-11 18:25:35 -0600

    Add anime.js for animations in Technologies page and update Navbar z-index

--- src/Pages/Technologies.tsx
+++ src/Pages/Technologies.tsx
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
 import TechnologyCard from "../Components/TechnologyCard";
 import technologies from "../technologies.json";
+import anime from "animejs/lib/anime.es.js";
 
 export default function Technologies() {
+	useEffect(() => {
+		anime({
+			targets: ".technology-card",
+			translateX: [200, 0],
+			opacity: [0, 1],
+			delay: anime.stagger(500),
+			easing: "easeOutQuad"
+		});
+	}, []);
+
 	return (
-		<div className="container mx-auto dark:text-white" id="technologies">
+		<div
+			className="container mx-auto dark:text-white relative"
+			id="technologies">
 			<h1 className="text-center font-bold text-4xl my-3">
 				Technologies and Tools
 			</h1>
-			<div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+			<div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 technology-card">
 				{technologies.map((technology, index) => {
 					return <TechnologyCard key={index} {...technology} />;
 				})}
