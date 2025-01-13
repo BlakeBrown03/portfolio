42fe83f9332b349b30b1aa27f34e8c933f134203
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-01-12 18:03:08 -0600

    Add Pollinations dependency and implement dynamic image generation in Home component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,29 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Typed from "typed.js";
 import "./Home.css";
+import { usePollinationsImage } from "@pollinations/react";
 
 export default function Home() {
 	const jobTitles = useRef(null);
+	const [seed, setSeed] = useState(Math.floor(Math.random() * 10000 + 1));
+	const generatedImage = usePollinationsImage(
+		"blonde, male programmer with sunglasses on sitting in his apartment coding with beautiful Chicago skyline in the background",
+		{
+			width: 500,
+			height: 500,
+			seed: seed,
+			model: "flux",
+			nologo: true
+		}
+	);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setSeed(Math.floor(Math.random() * 10000));
+		}, 20000);
+
+		return () => clearInterval(interval);
+	}, []);
 
 	useEffect(() => {
 		const typed = new Typed(jobTitles.current, {
@@ -21,11 +41,15 @@ export default function Home() {
 		<div
 			id="home"
 			className="container mx-auto grid grid-cols-1 md:grid-cols-2 dark:text-white">
-			<img
-				src="./picture-of-me.jpg"
-				alt="Picture of me"
-				className="rounded-full my-10"
-			/>
+			{generatedImage ? (
+				<img
+					src={generatedImage}
+					alt="Generated Image"
+					className="rounded-full my-10"
+				/>
+			) : (
+				<p>Loading...</p>
+			)}
 			<div className="flex flex-col justify-center items-center">
 				<h1 className="text-5xl text-blue-500 font-metamorphous">
 					Blake Brown
